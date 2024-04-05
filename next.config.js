cc78a6532b281a9ecdc7317b9378551daccf28f8
Bogdan Vasilev <bogdanvasilev@MacBook-Pro-Bogdan.local>
2024-04-05 17:55:07 +0300

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // basePath: "/travel-app",
+    // assetPrefix: "/travel-app/",
+    output: "export",
+    distDir: "dist",
+    images: {
+        unoptimized: true,
+    }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
